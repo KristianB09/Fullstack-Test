c6f86923733a75d94d9743818aa04deab5d03c7e
KristianB09 <kristianb09.kodehode@gmail.com>
2024-04-04 14:34:55 +0200

    Added async/await to controller to improve performance

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f606bdb50a8d141cb727adbc40286f262aa4a1c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2fa3241711c9a7b5e5d083cd9e42b47002dc7da")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
