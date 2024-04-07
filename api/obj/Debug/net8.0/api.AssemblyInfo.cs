3bf3bd1d0e4cb236bf3e3e63227ae48f3c2e4e89
KristianB09 <kristianb09.kodehode@gmail.com>
2024-04-07 16:04:53 +0200

    Added frontend basics

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6f86923733a75d94d9743818aa04deab5d03c7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5beecb9c38abf16a046f43f763c49c25444125f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
