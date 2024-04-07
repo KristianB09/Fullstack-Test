524a8cf37ed372ae6b45f92f3dbcdbbc9d9392a7
KristianB09 <kristianb09.kodehode@gmail.com>
2024-04-07 18:54:54 +0200

    Added basic comment logic for API

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5beecb9c38abf16a046f43f763c49c25444125f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf3bd1d0e4cb236bf3e3e63227ae48f3c2e4e89")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
