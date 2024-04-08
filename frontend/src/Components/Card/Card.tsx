02eaad9e5d207e50836b16ff5262477a4bb84099
kristianb09 <kristianb09.kodehode@gmail.com>
2024-04-08 12:29:57 +0200

    Restarted project with Vite as create-react-app is deprecated.

---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,26 @@
+import "./Card.css";
+
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
+
+const Card = ({ companyName, ticker, price }: Props) => {
+  return (
+    <div className="card">
+      <div className="details">
+        <h2>
+          {companyName} ({ticker})
+        </h2>
+        <p>${price}</p>
+      </div>
+      <p className="info">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo,
+        expedita!
+      </p>
+    </div>
+  );
+};
+
+export default Card;
